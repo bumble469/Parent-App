2e9e266423b97ddfd2aeac5581d136cb404b6f6c
unknown <raminajatin611@gmail.com>
2024-08-30 00:48:43 +0530

    added graphs in performance

--- src/examples/Charts/DoughnutCharts/DefaultDoughnutChart/configs/index.js
+++ src/examples/Charts/DoughnutCharts/DefaultDoughnutChart/configs/index.js
@@ -15,60 +15,34 @@ Coded by www.creative-tim.com
 
 /* eslint-disable no-dupe-keys */
 // Material Dashboard 2 React base styles
-import colors from "assets/theme/base/colors";
-
-const { gradients, dark } = colors;
-
-function configs(labels, datasets, cutout = 60) {
-  const backgroundColors = [];
-
-  if (datasets.backgroundColors) {
-    datasets.backgroundColors.forEach((color) => {
-      if (gradients[color]) {
-        if (color === "info") {
-          backgroundColors.push(gradients.info.main);
-        } else {
-          backgroundColors.push(gradients[color].state);
-        }
-      } else {
-        backgroundColors.push(dark.main);
-      }
-    });
-  } else {
-    backgroundColors.push(dark.main);
-  }
-
+export default function configs(labels, datasets) {
   return {
     data: {
       labels,
-      datasets: [
-        {
-          label: datasets.label,
-          weight: 9,
-          cutout,
-          tension: 0.9,
-          pointRadius: 2,
-          borderWidth: 2,
-          backgroundColor: backgroundColors,
-          fill: false,
-          data: datasets.data,
-        },
-      ],
+      datasets,
     },
     options: {
       responsive: true,
-      maintainAspectRatio: false,
       plugins: {
         legend: {
-          display: false,
+          position: 'top',
+        },
+        tooltip: {
+          callbacks: {
+            label: function(tooltipItem) {
+              return `${tooltipItem.dataset.label}: ${tooltipItem.raw}`;
+            },
+          },
         },
       },
-      interaction: {
-        intersect: false,
-        mode: "index",
+      scales: {
+        x: {
+          beginAtZero: true,
+        },
+        y: {
+          beginAtZero: true,
+        },
       },
     },
   };
 }
-
-export default configs;
